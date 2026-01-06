ddb7b6f3037a8091aef4bda2e2caf6094260f441
Wudi761 <rus.rikingener@gmail.com>
2026-01-06 16:33:30 +0300

    Add files via upload

---
+++ Lesson_4.2.py
@@ -0,0 +1,12 @@
+a = input()
+b = list(a)
+a1 = float(a[0])
+a2 =  float(a[1])
+a3 =  float(a[2])
+a4 =  float(a[3])
+a5 =  float(a[4])
+b1 = a4 ** a5
+b2 = b1 * a3
+b3 = a1 - a2
+b4 = b2 / b3
+print(b4)
